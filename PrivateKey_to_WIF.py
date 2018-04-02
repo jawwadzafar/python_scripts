88583abaa6afa6d2bc538c4e9e1d17ac09bbd2c5
Jawwad Zafar <zafarjawwad@gmail.com>
2018-04-02 13:31:53 +0530

    PrivateKey to WIF

---
+++ PrivateKey_to_WIF.py
@@ -0,0 +1,18 @@
+import hashlib
+import base58
+import binascii
+
+private_key_static = raw_input("Enter Your Private Key : ");
+
+extended_key = "80"+private_key_static
+first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+
+# add checksum to end of extended key
+final_key = extended_key+second_sha256[:8]
+
+# Wallet Import Format = base 58 encoded final_key
+WIF = base58.b58encode(binascii.unhexlify(final_key))
+
+print ("Your WIF Key : " + WIF)
+
